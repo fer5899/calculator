9eb07494c5d682cf3db00750a5449936981a1e60
fer5899 <fdometallic@gmail.com>
2022-10-09 11:38:23 +0200

    Change sqrt to be any root

--- functions.js
+++ functions.js
@@ -15,11 +15,12 @@ function multiply(a, b) {
     return a * b;
 }
 
-function sqrt(a) {
-    if(a < 0) return "ERROR";
-    return Math.sqrt(a);
+function root(a, b) {
+    if(a < 0 || b === 0) return "ERROR";
+    return Math.pow(a, 1/b);
 }
 
-function pow(a) {
-    return a ** 2;
+function pow(a, b) {
+    return Math.pow(a, b);
 }
+
