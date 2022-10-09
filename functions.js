82a29158d964be5014fce1b967ce8301acd88f51
fer5899 <fdometallic@gmail.com>
2022-10-09 12:05:05 +0200

    Modify operation functions to return also text

--- functions.js
+++ functions.js
@@ -1,27 +1,27 @@
 function add(a, b) {
-    return a + b;
+    return [a + b, `${a} + ${b} =`];
 }
 
 function substract(a, b) {
-    return a - b;
+    return [a - b, `${a} - ${b} =`];
 }
 
 function divide(numer, denom) {
     if(denom === 0) return "ERROR";
-    return numer / denom;
+    return [numer / denom, `${a} ÷ ${b} =`];
 }
 
 function multiply(a, b) {
-    return a * b;
+    return [a * b, `${a} ✕ ${b} =`];
 }
 
 function root(a, b) {
     if(a < 0 || b === 0) return "ERROR";
-    return Math.pow(a, 1/b);
+    return [Math.pow(a, 1/b), `${b}√${a} =`];
 }
 
 function pow(a, b) {
-    return Math.pow(a, b);
+    return [Math.pow(a, b), `${a}^${b} =`];
 }
 
 function operate(operation, a, b) {
